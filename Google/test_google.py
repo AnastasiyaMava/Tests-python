db5c2ebd7f2e257ba0804c27584ed65d0754b7ec
AnastasiyaMava <matyshava@gmail.com>
2022-05-30 20:25:12 +0300

    1

---
+++ Google/test_google.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import pytest
+
+
+@pytest.fixture(scope='class')
+def web_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path='/home/anastasiya'
+                                              '/PycharmProjects/pythonProject/Test/Google/chromedriver')
+    driver.maximize_window()
+    driver.get("https://www.google.com/")
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+class TestGoogle:
+
+    def test_url(self, web_driver):
+        current_url = web_driver.current_url
+        assert current_url == "https://www.google.com/"
